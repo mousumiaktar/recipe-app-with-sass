9763023d86eb3611a65d1ab63cd08886706ad0dd
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-06-29 22:16:17 +0600

    tabs without design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header"
+import Tabs from "./components/Tabs"
 
 // import './App.css'
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
      <Header />
+     <Tabs />
     </div>
   )
 }
