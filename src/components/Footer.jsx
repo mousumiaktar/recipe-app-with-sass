7966e4cd4e354d01d26285ed156fb287bc4f83dd
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-07-07 18:59:11 +0600

    logo change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,23 +5,23 @@ const Footer = () => {
         <div className="footer-area">
             <div className="container">
                 <div className="footer-body">
-                    <div className='footer-content'>
-                        <img  src="https://cdn.vectorstock.com/i/preview-1x/67/14/pharmacy-bio-and-naturel-recipe-logo-design-vector-25516714.jpg" alt="" />
+                    <div className='footer-flex'>
+                        <img  src="https://i.ibb.co/k4VP74Y/download.jpg" alt="" />
                     </div>
-                    <div className="popular-recipe">
+                    <div className="footer-flex">
                         <p>Popular Recipe</p>
                         <p><small>Pizza</small></p>
                         <p><small>Burger</small></p>
                         <p><small>Noodles</small></p>
                     </div>
-                    <div className="contact">
+                    <div className="footer-flex">
                         <p>Contact</p>
                         <p><small>Phone: 01799999999</small></p>
                         <p><small>Whatsapp: 01799999999</small></p>
                         <p><small>Email: recipe@gmail.com</small></p>
                     </div>
-                    <div className="allreripe">
-                        <p>Contact</p>
+                    <div className="footer-flex">
+                        <p>Recipe</p>
                         <p><small>Pizza</small></p>
                         <p><small>Burger</small></p>
                         <p><small>Noodles</small></p>
